9be4fc5d7a904a53cc71507a6b696406736f87f7
Chadwick McChadwick <chadwickmcchadwick@gmail.com>
2020-09-28 12:19:08 -0600

    Fix comparison address in fioGetAddress test

--- integration/src/fio/fio.ts
+++ integration/src/fio/fio.ts
@@ -31,26 +31,21 @@ export function fioTests(get: () => { wallet: HDWallet; info: HDWalletInfo }): v
       });
     }, TIMEOUT);
 
-    test(
+    test.only(
       "fioGetAddress()",
       async () => {
         if (!wallet) return;
-        let res = await wallet.fioGetAddress({
-          addressNList: bip32ToAddressNList("m/44'/235'/0'/0/0"),
-          showDisplay: false,
-        });
-        console.log(res)
         expect(
           await wallet.fioGetAddress({
             addressNList: bip32ToAddressNList("m/44'/235'/0'/0/0"),
             showDisplay: false,
           })
-        ).toEqual("FIO5kJKNHwctcfUM5XZyiWSqSTM5HTzznJP9F3ZdbhaQAHEVq575o");
+        ).toEqual("FIO6iLE1J4zb2SyDGTH9d6UL9Qm6hhqRce27QvP8AKxVLASGhtm7z");
       },
       TIMEOUT
     );
 
-    test(
+    test.only(
       "fioSignTx()",
       async () => {
         if (!wallet) return;
@@ -63,6 +58,7 @@ export function fioTests(get: () => { wallet: HDWallet; info: HDWalletInfo }): v
                 payee_public_key: "FIO7MpYCsLfjPGgXg8Sv7usGAw6RnFV3W6HTz1UP6HvodNXSAZiDp",
                 amount: "1000000000",
                 max_fee: 800000000000,
+                tpid: "",
               },
             },
           ],
--- packages/hdwallet-native/src/fio.ts
+++ packages/hdwallet-native/src/fio.ts
@@ -1,5 +1,6 @@
 import * as core from "@mcchadwick/hdwallet-core";
-const fio = require("@fioprotocol/fiosdk");
+import * as fio from "fiosdk-offline";
+//const fio = require("fiosdk-offline");
 const fetch = require("node-fetch");
 
 const fetchJson = async (uri, opts = {}) => {
@@ -41,15 +42,15 @@ export function MixinNativeFioWalletInfo<TBase extends core.Constructor>(Base: T
 export function MixinNativeFioWallet<TBase extends core.Constructor>(Base: TBase) {
   return class MixinNativeFioWallet extends Base {
     _supportsFio = true;
-    baseUrl = "https://testnet.fioprotocol.io:443/v1/";
+    baseUrl = "https://fio.eu.eosamsterdam.net/v1/";
     #seed = "";
     #privateKey = "";
     #publicKey = "";
-    #fioSdk;
+    #fioSdk: any;
 
-    async fioInitializeWallet(seed: string): Promise<string> {
+    async fioInitializeWallet(seed: string): Promise<void> {
       this.#seed = seed;
-      return await this.fioSDKInit(seed);
+      await this.fioSDKInit(seed);
     }
 
     async fioGetAddress(msg: core.FioGetAddress): Promise<string> {
@@ -59,16 +60,20 @@ export function MixinNativeFioWallet<TBase extends core.Constructor>(Base: TBase
     }
 
     async fioSignTx(msg: core.FioSignTx): Promise<core.FioSignedTx> {
-      const account: string = msg.actions[0].account || "fio.token";
-      const action: string = msg.actions[0].name || "fiotrnspubky";
+      const account: string = msg.actions[0].account;
+      const action: string = msg.actions[0].name;
       const data: core.Fio.FioTxActionData = msg.actions[0].data;
+      console.log("**ACCOUNT**", account);
+      console.log("**ACTION**", action);
+      console.log("**DATA**", data);
       if (!this.#fioSdk) {
         // Throw error. fioInitializeWallet has not been called.
       }
-      const res = this.#fioSdk.prepareTransaction(account, action, msg.actions[0].data);
+      const res = await this.#fioSdk.prepareTransaction(account, action, data);
       if (!res.signatures || !res.packed_trx) {
         // Throw error. Transaction is invalid.
       }
+      console.log("**RES**", res);
       const sig = {
         serialized: res.packed_trx, // Serialized hexadecimal transaction
         signature: res.signatures[0], //
@@ -77,9 +82,8 @@ export function MixinNativeFioWallet<TBase extends core.Constructor>(Base: TBase
       return sig;
     }
 
-    async fioSDKInit(seed: string, path?: string): Promise<string>{
+    async fioSDKInit(seed: string, path?: string) {
       const privateKeyRes = await fio.FIOSDK.createPrivateKeyMnemonic(seed, path);
-      return privateKeyRes;
       this.#privateKey = privateKeyRes.fioKey;
       const publicKeyRes = fio.FIOSDK.derivedPublicKey(this.#privateKey);
       this.#publicKey = publicKeyRes.publicKey;
